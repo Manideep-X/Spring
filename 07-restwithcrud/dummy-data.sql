6f1e1d8b5c188661a2e7ba246235a09954da76fc
MandyX <deepanddipyo@gmail.com>
2025-05-26 02:09:25 +0530

    dummy data added

--- 07-restwithcrud/dummy-data.sql
+++ 07-restwithcrud/dummy-data.sql
@@ -1,7 +1,17 @@
 USE `employee_tracker`;
 INSERT INTO `employee` VALUES 
-	(10,'Ruth','Bond','ruth.bond@gmail.com'),
-	(11,'Sally','Gill','sally.gill@gmail.com'),
-	(12,'Richard','Davidson','richard.davidson@gmail.com'),
-	(13,'Grace','Paige','grace.paige@gmail.com'),
-	(14,'Gordon','Hart','gordon.hart@gmail.com');
+	(1,'Ruth','Bond','ruth.bond@gmail.com'),
+	(2,'Sally','Gill','sally.gill@gmail.com'),
+	(3,'Richard','Davidson','richard.davidson@gmail.com'),
+	(4,'Grace','Paige','grace.paige@gmail.com'),
+    (5,'Nicola','Mackenzie','nicola.mackenzie@gmail.com'),
+    (6,'Megan','White','megan.white@gmail.com'),
+    (7,'Natalie','Robertson','natalie.robertson@gmail.com'),
+    (8,'Christian','Dickens','christian.dickens@gmail.com'),
+    (9,'Samantha','Miller','samantha.miller@gmail.com'),
+    (10,'Victor','Robertson','victor.robertson@gmail.com'),
+    (11,'Sean','Churchill','sean.churchill@gmail.com'),
+    (12,'Rachel','Paige','rachel.paige@gmail.com'),
+    (13,'Pippa','Burgess','pippa.burgess@gmail.com'),
+    (14,'Zoe','Sharp','zoe.sharp@gmail.com'),
+	(15,'Gordon','Hart','gordon.hart@gmail.com');
