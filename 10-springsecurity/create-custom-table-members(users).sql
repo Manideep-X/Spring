bdd91acf5337d65b64c94870938645feefdd2781
Manideep <deepanddipyo@gmail.com>
2025-06-04 14:57:00 +0530

    custom user and authority tables are added

---
+++ 10-springsecurity/create-custom-table-members(users).sql
@@ -0,0 +1,15 @@
+USE `employee_tracker`;
+
+DROP TABLE IF EXISTS `members`;
+CREATE TABLE `members` (
+    `name` varchar(50) NOT NULL,
+	`pass` char(68) NOT NULL,
+	`active` tinyint NOT NULL,
+    
+	PRIMARY KEY(`name`)
+);
+
+INSERT INTO `members` VALUES 
+	('john','{bcrypt}$2a$12$5PU9kPFZg4tBXUeMCskkgOZExN7Sko4NC3bMPIkQJ3druwLgcQohu',1),
+    ('mary','{bcrypt}$2a$12$cP1yf9RYvirBCku0jqaI2.jy4JBy6da4BBP2tVi8lfkHWP5.h3aPi',1),
+    ('susan','{bcrypt}$2a$12$gACTANgUv/tJ/c2GtMh23OJFGQrL0pamCVXnFJXYH3h6Yog/xDCO2',1);
