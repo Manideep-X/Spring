dbc52821dc8155afa4eaf78978be86f1e0c845c1
Manideep <deepanddipyo@gmail.com>
2025-06-07 23:53:02 +0530

    Simple form using Spring & Thymeleaf is initialised

---
+++ 12-springmvcform/src/test/java/com/twelfth/project/springmvcform/SpringmvcformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twelfth.project.springmvcform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
