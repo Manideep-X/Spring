3e0f6b220e1a9fbb127bea23078aed6dbe024ff4
Manideep <deepanddipyo@gmail.com>
2025-06-07 20:15:52 +0530

    Guide about the main endpoint of REST repository is added

--- 09-springdatarest/src/main/java/com/ninthjava/project/springdatarest/repository/EmpRepoRestInterface.java
+++ 09-springdatarest/src/main/java/com/ninthjava/project/springdatarest/repository/EmpRepoRestInterface.java
@@ -5,6 +5,8 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.ninthjava.project.springdatarest.model.EmployeeEntity;
 
+// The path specifies the URL under which the repository will be exposed as a REST endpoint.
+// For this the main URL will be: http://localhost:8080/employees
 @RepositoryRestResource(path = "employees")
 public interface EmpRepoRestInterface extends JpaRepository<EmployeeEntity, Integer> {
 
