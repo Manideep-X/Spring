fbd9735faccf7e7a5cfc38c8304052edca1830b4
Manideep <deepanddipyo@gmail.com>
2025-06-16 21:10:43 +0530

    Spring MVC Security with Thymeleaf is created

---
+++ 13-springmvcsecurity/src/main/java/com/thirteenth/project/springmvcsecurity/security/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.thirteenth.project.springmvcsecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+
+        UserDetails john = User.builder()
+                .username("john")
+                .password("{noop}123")
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails mary = User.builder()
+                .username("mary")
+                .password("{noop}123")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+
+        UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}123")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.authorizeHttpRequests(configurer -> configurer
+            .anyRequest().authenticated() // any request to this app must be authenticated
+        
+        ).formLogin(form -> form
+            .loginPage("/login")
+            .loginProcessingUrl("/authenticate")
+            .permitAll() // it is used to show the login page to everyone without login
+        );
+
+        return httpSecurity.build();
+
+    }
+
+}
