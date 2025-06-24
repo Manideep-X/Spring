609115a265ba07fcb9edf77c3439172a809e5015
Manideep <deepanddipyo@gmail.com>
2025-06-24 23:30:08 +0530

    Advance mapping in JPA/Hibernate in Spring project files are added

---
+++ 14-advancemapping/src/main/java/com/fourteenth/project/advancemapping/AdvancemappingApplication.java
@@ -0,0 +1,13 @@
+package com.fourteenth.project.advancemapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancemappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancemappingApplication.class, args);
+	}
+
+}
---
+++ 14-advancemapping/src/test/java/com/fourteenth/project/advancemapping/AdvancemappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourteenth.project.advancemapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancemappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
