1ec186e826a0ebb1e4b1e3256a5134c57c8ae052
Manideep <deepanddipyo@gmail.com>
2025-07-08 00:59:27 +0530

    course table along with it's attributes are added

---
+++ 14-advancemapping/create-course-table.sql
@@ -0,0 +1,11 @@
+USE `manage_edu_db`;
+
+CREATE TABLE `course` (
+	`id` int NOT NULL AUTO_INCREMENT,
+    `title` varchar(128) DEFAULT NULL,
+    `instructor_id` int DEFAULT NULL,
+    
+    PRIMARY KEY (`id`),
+    UNIQUE KEY (`title`),
+    FOREIGN KEY (`instructor_id`) REFERENCES `instructor` (`id`)
+) AUTO_INCREMENT = 1 ;
--- 14-advancemapping/src/main/java/com/fourteenth/project/advancemapping/model/Instructor.java
+++ 14-advancemapping/src/main/java/com/fourteenth/project/advancemapping/model/Instructor.java
@@ -1,5 +1,7 @@
 package com.fourteenth.project.advancemapping.model;
 
+import java.util.List;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -7,6 +9,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
@@ -31,6 +34,10 @@ public class Instructor {
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "instructor_detail_id")
     private InstructorDetail instructorDetail;
+
+    @OneToMany(mappedBy = "instructor", 
+                cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    private List<Course> courses;
 
     public Instructor() {
     }
